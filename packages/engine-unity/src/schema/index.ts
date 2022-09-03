34049dc18caf82bcbe38abdc0610724a33c33521
Pavel Horal <pavel.horal@orchitech.cz>
2022-09-03 21:07:42 +0200

    Add support for simple TextAsset.

--- packages/engine-unity/src/schema/index.ts
+++ packages/engine-unity/src/schema/index.ts
@@ -3,11 +3,13 @@ import { EngineConfig } from '../EngineConfig.js';
 import { default as BaseTypes } from './BaseScript.js';
 import { default as BaseScript } from './BaseTypes.js';
 import { default as LanguageSourceAsset } from './LanguageSourceAsset.js';
+import { default as TextAsset } from './TextAsset.js';
 
 export function registerTypes(config: EngineConfig): TypeRegistry {
   return {
     ...BaseTypes(),
     ...BaseScript(),
-    ...LanguageSourceAsset(config)
+    ...LanguageSourceAsset(config),
+    ...TextAsset(),
   };
 }
